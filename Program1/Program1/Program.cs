3bf9d35ff019f3a834aaaf60b4735e22b2902871
ktokarczyk <cris.tokarczyk@gmail.com>
2022-05-07 21:41:21 +0200

    dodanie projektow do repozytorium

---
+++ Program1/Program1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Program1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //// zmienne liczbowe w rogramowaniu 
+            //int liczba = 4 / 3;
+            //float liczbaZmiennoprzecinkowa = 4.0f / 3;
+            //double liczbaZmiennoprzecinkowaPodwojnejDokladnosci = 4.0 / 3;
+
+            //bool wartoscBoolowska = false;
+            //bool isSleep = true;
+
+            //Console.WriteLine($"Moje liczby: ");
+            //Console.WriteLine($"{nameof(liczba)} = {liczba}");
+            //Console.WriteLine($"{nameof(liczbaZmiennoprzecinkowa)} = {liczbaZmiennoprzecinkowa}");
+            //Console.WriteLine($"{nameof(liczbaZmiennoprzecinkowaPodwojnejDokladnosci)} = {liczbaZmiennoprzecinkowaPodwojnejDokladnosci}");
+            //Console.WriteLine($"{nameof(wartoscBoolowska)} = {wartoscBoolowska}");
+
+
+            //// zmienne przechowujące znaki
+
+            char znak = 'c';
+            string ciagZnakow = "RAV 4";
+            float pojSil = 2.0f;
+
+
+            Console.WriteLine($"Moje znaki: ");
+            Console.WriteLine($"{nameof(znak)} = {znak}");
+            Console.WriteLine($"{nameof(ciagZnakow)} = {ciagZnakow}");
+
+
+            // tablice i kolekcje
+
+            //int[] zbiorLiczb = { 7, 10, 22, 3, 5 };
+            //Console.WriteLine(zbiorLiczb[2]);
+
+            char[] tablicaCharow = { 'n', 'i', 'e', 'c', 'h', ' ', 'm', 'o', 'c', ' ', 'b', 'ę', 'd', 'z', 'i', 'e', ' ', 'z', ' ', 't', 'o', 'b', 'ą' };
+            //Console.WriteLine(tablicaCharow);
+
+            //List<string> listaZdan = new List<string>();
+
+            //listaZdan.Add(ciagZnakow);
+
+            //Console.WriteLine(listaZdan[1]);  
+
+            //for (int i = 0; i < tablicaCharow.Length; i++) // i++ => i = i + 1; | ++i 
+            //{
+            //    Console.Write(tablicaCharow[i]);
+            //}
+
+
+            //int i = 0;
+            //while (i < tablicaCharow.Length)
+            //{
+            //    Console.Write(tablicaCharow[i]);
+            //    i++;
+            //}
+
+            foreach (char cc in tablicaCharow)
+            {
+                Console.Write(cc);
+            }
+
+
+
+        }
+    }
+}
---
+++ Program2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace Program2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // instrukcje warunkowe 
+           // #region Instrukcje warunkowe
+            string poleTekstowe = "Coś tu jest";
+
+            //if (string.IsNullOrEmpty(poleTekstowe))
+            //{
+            //    Console.WriteLine("Pole tekstowe jest puste!");
+            //}
+
+            //if (string.IsNullOrEmpty(poleTekstowe))
+            //{
+            //    Console.WriteLine("Pole tekstowe jest puste!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine(poleTekstowe);
+            //}
+            //#endregion
+
+            //// bardziej złożone instrukcje warunkowe
+            //#region Bardziej zaawansowane instrukcje 
+            //int liczbaTabliczekCzekolady = 0;
+
+            
+            //if (liczbaTabliczekCzekolady == 1)
+            //{
+            //    Console.WriteLine("O nie jest tylko jedna tabliczka czekolady :< ");
+            //}
+            //else if (liczbaTabliczekCzekolady == 2)
+            //{
+            //    Console.WriteLine("Są dwie tabliczki czekolady. Huraa! ");
+            //}
+            //else if (liczbaTabliczekCzekolady == 3)
+            //{
+            //    Console.WriteLine("Woow! Mmay trzy tabliczki czekolady!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Już straciłem rachubę!");
+            //}
+
+            //// switch coś bardzo podobnego do zapytań
+            //#region Switch
+
+
+
+            //switch (liczbaTabliczekCzekolady)
+            //{
+            //    case 1:
+            //        Console.WriteLine("O nie jest tylko jedna tabliczka czekolady :< ");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Są dwie tabliczki czekolady. Huraa! ");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Woow! Mmay trzy tabliczki czekolady!");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Już straciłem rachubę!");
+            //        break;
+            //}
+            //#endregion
+
+
+
+        }
+    }
+}
